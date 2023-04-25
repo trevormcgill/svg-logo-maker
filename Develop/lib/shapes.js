c5ba90da35f23d5d8a8f856becc540c265bec5f7
Trevor McGill <trevor.mcgill@icloud.com>
2023-04-24 21:50:01 -0500

    initial commit, adding files

---
+++ Develop/lib/shapes.js
@@ -0,0 +1,18 @@
+// Base class for shapes
+class Shape {
+
+}
+
+class Circle extends Shape {
+
+}
+
+class Triangle extends Shape {
+
+}
+
+class Square extends Shape {
+
+}
+
+module.exports = { Circle, Triangle, Square };
